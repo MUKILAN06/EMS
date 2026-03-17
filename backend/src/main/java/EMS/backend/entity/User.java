e41cb84fa77d4869f3663782b71c19705933aaf6
2k22cse071-design <2k22cse071@kiot.ac.in>
2026-03-17 23:05:06 +0530

    created User,Role,Employee entites

--- backend/src/main/java/EMS/backend/entity/User.java
+++ backend/src/main/java/EMS/backend/entity/User.java
@@ -1,5 +1,32 @@
 package EMS.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private boolean verified = false;
 }
