1907f348377f4383b5e710a64e17577b791a2792
2k22cse071-design <2k22cse071@kiot.ac.in>
2026-03-17 23:42:32 +0530

    Completed backend files for Auth+Security

---
+++ backend/src/main/java/EMS/backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package EMS.backend.repository;
+
+import EMS.backend.entity.User;
+import EMS.backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+    List<User> findByRole(Role role);
+}
