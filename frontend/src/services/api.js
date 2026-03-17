598aaa7a54f85c41cd2982a91d685d805bf62070
MUKILAN06 <pjmukilan2004@gmail.com>
2026-03-17 23:53:34 +0530

    Configure environment variables for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
